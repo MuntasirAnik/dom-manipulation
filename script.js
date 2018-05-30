e16ad6a46ad42b348d4fb3cbfdaa25f69167a4c5
MuntasirAnik <anik.muntasir005@gmail.com>
2018-05-30 04:19:31 +0000

    classwork 4 2a

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 /* Task 1 */
-
+var heading = document.getElementById("heading")
+console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
-
-
+//task 2a
+var textBtn = document.getElementById('textBtn2')
+textBtn.addEventListener('click',function(){
+   var task2 = document.getElementById('task2a') 
+   var paragraph = document.createElement('p')
+   paragraph.innerText = 'Hello world'
+   task2.appendChild(paragraph)
+})
 
 /* Task 4 using jQuery */
